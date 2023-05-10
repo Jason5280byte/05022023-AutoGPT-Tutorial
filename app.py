12b0b006f07e997245e6b2a926c6c36202a90185
Jason5280byte <81045782+Jason5280byte@users.noreply.github.com>
2023-05-09 21:20:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,50 +11,47 @@ from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-#App Framework
-st.title('YoutubeGPT')
-prompt = st.text_input('Plug in your prompt here')
+# App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here') 
 
-
-#Prompt Templates
+# Prompt templates
 title_template = PromptTemplate(
-    input_variables = ['topic'],
-    template='write me a youtube title about {topic}'
+    input_variables = ['topic'], 
+    template='write me a youtube video title about {topic}'
 )
 
 script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'],
-    template='write me a youtube video outline based on this title TITLE: {title} while leveraging this wikipedia research: {wikipedia_research}'
+    input_variables = ['title', 'wikipedia_research'], 
+    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
 )
 
-
-#Memory
+# Memory 
 title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
-#LLMs
-llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key ='title', memory=title_memory)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key = 'script', memory=script_memory)
+
+# Llms
+llm = OpenAI(temperature=0.9) 
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
 wiki = WikipediaAPIWrapper()
 
-#Prompt Response to Screen
+# Show stuff to the screen if there's a prompt
 if prompt: 
-  title = title_chain.run(prompt)
-  wiki_research = wiki.run(prompt)
-  script = script_chain.run(title=title, wikipedia_research = wiki_research)
-  
-  st.write(title)
-  st.write(script)
+    title = title_chain.run(prompt)
+    wiki_research = wiki.run(prompt) 
+    script = script_chain.run(title=title, wikipedia_research=wiki_research)
 
-#History on Screen
-with st.expander('Title History'):
-    st.info(title_memory.buffer)
+    st.write(title) 
+    st.write(script) 
 
-with st.expander('Script History'):
+    with st.expander('Title History'): 
+        st.info(title_memory.buffer)
+
+    with st.expander('Script History'): 
         st.info(script_memory.buffer)
 
-with st.expander('Wikipedia Research'):
+    with st.expander('Wikipedia Research'): 
         st.info(wiki_research)
-    
